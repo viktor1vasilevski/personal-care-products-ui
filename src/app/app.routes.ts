dbd547b23e833fda6278d162fe100ad62836a9e5
vik_vas <vasilevskiviktor@yahoo.com>
2024-10-30 21:31:37 +0100

    remove imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { SoapsBeardComponent } from './pages/customer/soaps/beard/soaps-beard/soaps-beard.component';
-import { ProductComponent } from './pages/admin/product/product.component';
 import { authGuard } from './core/guards/auth.guard';
 import { UnauthorizedComponent } from './shared/unauthorized/unauthorized.component';
 import { HomeComponent } from './core/components/home/home.component';
-import { SubcategoryComponent } from './pages/admin/subcategory/subcategory.component';
 import { CategoryComponent } from './pages/admin/category/category.component';
 
 export const routes: Routes = [
